8fe327d3c5b4c32ec06c2f2af898f94edad214f6
wily <103911498+botreaction@users.noreply.github.com>
2023-09-07 04:51:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -46,6 +46,10 @@ const owner = JSON.parse(fs.readFileSync('./database/owner.json'))
 
 const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) })
 
+///
+process.on('uncaughtException', console.error)
+///
+
 require('./XeonCheems8.js')
 nocache('../XeonCheems8.js', module => console.log(color('[ CHANGE ]', 'green'), color(`'${module}'`, 'green'), 'Updated'))
 require('./index.js')
